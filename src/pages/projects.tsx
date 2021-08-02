98007b38f917358d326736fb41e453509dc856ea
Vossryn <vossryn@gmail.com>
2021-08-01 22:31:20 -0400

    Adding in more work with recapcha and sendgrid

---
+++ src/pages/projects.tsx
@@ -0,0 +1,9 @@
+import Container from "../components/Container";
+
+export default function projects() {
+  return (
+    <Container>
+      <div className="text-2xl text-blue-200 text-center">Under Construction - Comming Soon</div>
+    </Container>
+  );
+}
