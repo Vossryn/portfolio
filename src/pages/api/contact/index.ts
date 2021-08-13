792540bbaaae4f0e3cec2171492b0cec80c2d057
Vossryn <vossryn@gmail.com>
2021-08-12 20:19:20 -0400

    Trying to fix a total screw up with my repo

--- src/pages/api/contact/index.ts
+++ src/pages/api/contact/index.ts
@@ -7,54 +7,6 @@ type ResponseData = {
   error: string | null;
 };
 
-interface sendEmailProps {
-  email: string;
-  subject: string;
-  message: string;
-}
-
-const SENDGRID_API: string = "https://api.sendgrid.com/v3/mail/send";
-
-const sendEmail = async ({ email, subject, message }: sendEmailProps) => {
-  try {
-    const response = await fetch(SENDGRID_API, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
-      },
-      body: JSON.stringify({
-        personalizations: [
-          {
-            to: [
-              {
-                email: "vossryn@gmail.com",
-              },
-            ],
-            subject,
-          },
-        ],
-        from: {
-          email,
-          name: "",
-        },
-        content: [
-          {
-            type: "text/html",
-            value: message,
-          },
-        ],
-      }),
-    });
-
-    console.log({ response });
-    return response;
-  } catch (error) {
-    console.log({ error });
-    throw error;
-  }
-};
-
 export default async (req: NextApiRequest, res: NextApiResponse<ResponseData>) => {
   try {
     if (req.method === "PUT") {
